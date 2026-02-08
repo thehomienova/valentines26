f2dc51d164b8667325e734348b575cd16d932015
thehomienova <thehomienova@gmail.com>
2026-02-07 21:29:05 -0800

    addes gifts and itinerary pages, working on image rotation

--- script.js
+++ script.js
@@ -5,18 +5,37 @@ let main = document.querySelector('.main');
 let question = document.getElementById('question');
 let buttons = document.querySelector('.buttons');
 let itinerary = document.querySelector('#itinerary');
-
-
+let home = document.querySelector('#home');
+let song = new Audio('audio/song.mp3');
 
 yes.addEventListener('click', () => {
     main.style.backgroundImage = 'url("images/yesbackground.jpg")';
     question.innerHTML = 'i love u <3';
     question.classList.add('said-yes');
     buttons.style.display = 'none';
-    itinerary.classList.add('said-yes');
-    textBubble.classList.add('said-yes');
+    song.currentTime = 111;
+    song.play();
+
+    setTimeout(() => {
+        main.style.backgroundImage = 'url("images/walking.jpeg")';
+        main.style.backgroundSize = 'cover';
+        main.style.transition = 'opacity 3s ease-out';
+        question.innerHTML = 'so whats next?';
+        question.style.top = '32vh';
+    }, 3000);
+
+    setTimeout(() => {
+        main.style.backgroundImage = 'url("images/hands.jpg")';
+        main.style.backgroundSize = 'cover';
+        main.style.transition = 'opacity 3s ease-out';
+        question.style.display = 'none';
+        itinerary.classList.add('said-yes');
+        gift.classList.add('said-yes');
+    }, 6000);
 
 });
+
+
 
 no.addEventListener('click', () => {
     main.style.backgroundImage = 'url("images/nobackground.jpg")';
